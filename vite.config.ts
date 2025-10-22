e97b880ebc22631a4b64cb5dcb7008b47cfcde42
saeun-park <ajsklaoao@naver.com>
2025-10-22 22:11:56 +0900

    에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // For GitHub User Pages (username.github.io)
+  base: '/saeun-park.github.io/',
   plugins: [react()],
 });
