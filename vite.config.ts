81e274d26b7541ba2856ed332238b7a52ceb671d
saeun-park <ajsklaoao@naver.com>
2026-01-18 23:52:17 +0900

    fix: restore project structure and clean build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 빌드 결과물은 무조건 dist 폴더 안으로만 들어가라고 명시
+  },
 });
