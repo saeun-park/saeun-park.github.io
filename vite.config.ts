b22a29e706a34a625c2f2c2bc59e77720a5fef4e
saeun-park <ajsklaoao@naver.com>
2026-01-18 23:59:09 +0900

    re-deploy: fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist', // 빌드 결과물은 무조건 dist 폴더 안으로만 들어가라고 명시
+    outDir: 'dist', // 빌드 결과물은 무조건 dist 폴더 안으로만 들어가라고 명시.
   },
 });
